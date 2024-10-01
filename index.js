200bfccf4faba8ba7d7a4fca0d68e41467565a18
Ian Kimutai <kimutaiian2@gmail.com>
2024-10-01 21:17:41 +0300

    update index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+function receivesAFunction(callback) {
+    callback();
+  }
+  
+  function returnsANamedFunction() {
+    function namedFunction() {
+      return "I am a named function";
+    }
+    return namedFunction;
+  }
+  
+  function returnsAnAnonymousFunction() {
+    return function() {
+      return "I am an anonymous function";
+    };
+  }
+  
